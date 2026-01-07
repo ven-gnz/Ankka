2596f47e9b7f17cf5a2624e62aa206ed75c7dc81
ven-gnz <kangas.otso@gmail.com>
2026-01-07 19:26:06 +0200

    Simple STB texture implementation for renderer

---
+++ Ankka/opengl/Texture.cpp
@@ -0,0 +1,42 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <STB/stb_image.h>
+#include "Texture.h"
+
+bool Texture::loadTexture(std::string textureFilename)
+{
+	int mTexWidth, mTexHeight, mNumberOfChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* textureData = stbi_load(textureFilename.c_str(), &mTexWidth, &mTexHeight, &mNumberOfChannels, 0);
+
+	if (!textureData)
+	{
+		stbi_image_free(textureData);
+		return false;
+	}
+
+	glGenTextures(1, &mTexture);
+	glBindTexture(GL_TEXTURE_2D, mTexture);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, mTexWidth, mTexHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData);
+
+	glGenerateMipmap(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	stbi_image_free(textureData);
+	return true;
+}
+
+void Texture::bind()
+{
+	glBindTexture(GL_TEXTURE_2D, mTexture);
+}
+
+void Texture::unbind()
+{
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
---
+++ Ankka/opengl/Texture.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+class Texture
+{
+	bool loadTexture(std::string filename);
+	void bind();
+	void unbind();
+	void cleanup();
+
+private:
+	GLuint mTexture = 0;
+	int mTexWidth = 0;
+	int mTexHeight = 0;
+	int mNumberOfChannels = 0;
+	std::string mTextureName;
+};
