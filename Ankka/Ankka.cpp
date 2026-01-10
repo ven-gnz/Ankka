190d251fc9f6bd393fc2b274de3751570bb152af
ven-gnz <kangas.otso@gmail.com>
2026-01-10 20:25:37 +0200

    Deferring vulkan renderer chapter to get to animations faster

--- Ankka/Ankka.cpp
+++ Ankka/Ankka.cpp
@@ -1,12 +1,15 @@
 ﻿#include "Ankka/Ankka.h"
 
+
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {	
 	std::unique_ptr<Window> w = std::make_unique<Window>();
 	// OpenGL window
-	if (!w->init(640, 480, "Test Window", true))
+	// I will be deferring the vulkan renderer development to get the relevant bits on the book faster
+	if (!w->init(640, 480, "Test Window", false))
 	{
 		Logger::log(1, "$s error: Window init error\n",
 			__FUNCTION__);
--- includes/vulkan/VkRenderer.h
+++ includes/vulkan/VkRenderer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+// Only a stub for now, I made the decision to focus on getting to the meat faster by deferring the development of this.
+
 #include <vulkan/vulkan.h>
 #include <GLFW/glfw3.h>
 #include <VkBootstrap.h>
