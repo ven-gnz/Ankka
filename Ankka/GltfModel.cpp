b6cbd75fec05832c30d796f4cc522877e272ef9f
ven-gnz <kangas.otso@gmail.com>
2026-02-08 17:20:03 +0200

    Removed redundant code

--- Ankka/GltfModel.cpp
+++ Ankka/GltfModel.cpp
@@ -348,22 +348,6 @@ void GltfModel::getNodes(std::shared_ptr<GltfNode> treeNode)
 
 }
 
-void GltfModel::getInvBindMatrices()
-{
-	const tinygltf::Skin& skin = mModel->skins.at(0);
-	int invBindMatAccessor = skin.inverseBindMatrices;
-
-	const tinygltf::Accessor& accessor = mModel->accessors.at(invBindMatAccessor);
-	const tinygltf::BufferView& bufferView = mModel->bufferViews.at(accessor.bufferView);
-	const tinygltf::Buffer& buffer = mModel->buffers.at(bufferView.buffer);
-
-	mInverseBindMatrices.resize(skin.joints.size());
-	mJointMatrices.resize(skin.joints.size());
-
-	std::memcpy(mInverseBindMatrices.data(), &buffer.data.at(0) + bufferView.byteOffset,
-		bufferView.byteLength);
-}
-
 bool GltfModel::loadModel(OGLRenderData& renderData,
 	std::string modelFileName,
 	std::string textureFileName)
