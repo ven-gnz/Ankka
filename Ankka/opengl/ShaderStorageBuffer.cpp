004bd22e1057f3759871c0ff4dc6985726be4ae2
ven-gnz <kangas.otso@gmail.com>
2026-03-08 14:56:50 +0200

    Simple override for dual quats

--- Ankka/opengl/ShaderStorageBuffer.cpp
+++ Ankka/opengl/ShaderStorageBuffer.cpp
@@ -33,6 +33,27 @@ void ShaderStorageBuffer::uploadSsboData(
 	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
 }
 
+void ShaderStorageBuffer::uploadSsboData(const std::vector<glm::mat2x4>& bufferData, int bindingPoint)
+{
+
+	size_t bufferSize = bufferData.size() * sizeof(glm::mat2x4);
+	if (bufferData.size() == 0)
+	{
+		Logger::log(1, "%s: trying to upload an empty ssbo buffer s\n",
+			__FUNCTION__);
+		return;
+	}
+
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, mShaderStorageBuffer);
+	glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, bufferSize, bufferData.data());
+	glBindBufferRange(GL_SHADER_STORAGE_BUFFER,
+		bindingPoint,
+		mShaderStorageBuffer,
+		0,
+		bufferSize);
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+}
+
 void ShaderStorageBuffer::cleanup()
 {
 	glDeleteBuffers(1, &mShaderStorageBuffer);
