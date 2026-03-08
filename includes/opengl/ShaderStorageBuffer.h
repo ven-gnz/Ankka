73ec486f242be9390b4405295d0558070a57202a
ven-gnz <kangas.otso@gmail.com>
2026-03-08 13:50:52 +0200

    Shader storage buffer object wrapper

---
+++ includes/opengl/ShaderStorageBuffer.h
@@ -0,0 +1,17 @@
+
+#pragma once
+#include <vector>
+#include <glm/glm.hpp>
+#include <glad/glad.h>
+
+class ShaderStorageBuffer
+{
+public:
+	void init(size_t bufferSize);
+	void uploadSsboData(std::vector<glm::mat4> bufferData, int bindingPoint);
+	void cleanup();
+
+private:
+	size_t mBufferSize;
+	GLuint mShaderStorageBuffer = 0;
+};
