41cfa2bcaaf0ab0b02d316305c845a7de61fc373
ven-gnz <kangas.otso@gmail.com>
2026-05-01 20:26:39 +0300

    accessors for translate, scale and rotate over time

--- Ankka/model/GltfAnimationChannel.cpp
+++ Ankka/model/GltfAnimationChannel.cpp
@@ -76,4 +76,193 @@ void GltfAnimationChannel::loadChannelData(
 			outputBufferView.byteLength);
 		setScalings(scale);
 	}
+
 }
+
+glm::vec3 GltfAnimationChannel::getTranslation(float time)
+{
+	if (mTranslations.size() == 0) return glm::vec3(1.0f);
+
+	if (time < mTimings.at(0)) return mTranslations.at(0);
+	if (time > mTimings.at(mTimings.size() - 1)) return mTranslations.at(mTranslations.size() - 1);
+
+	int prevTimeIndex = 0;
+	int nextTimeIndex = 0;
+	for (int i = 0; i < mTimings.size(); ++i)
+	{
+		if (mTimings.at(i) > time)
+		{
+			nextTimeIndex = i;
+			break;
+		}
+		prevTimeIndex = i;
+	}
+
+	if (prevTimeIndex == nextTimeIndex) return mTranslations.at(prevTimeIndex);
+
+	glm::vec3 finalTranslate = glm::vec3(0.0f);
+
+	switch (mInterType)
+	{
+	case EInterpolationType::STEP: {
+		finalTranslate = mTranslations.at(prevTimeIndex);
+	}
+		break;
+	case EInterpolationType::LINEAR:
+	{
+		float interpolatedTime = (time - mTimings.at(prevTimeIndex)) /
+			(mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex));
+
+		glm::vec3 prevTranslate = mTranslations.at(prevTimeIndex);
+		glm::vec3 nextTranslate = mTranslations.at(nextTimeIndex);
+
+		finalTranslate = prevTranslate + interpolatedTime *
+			(nextTranslate - prevTranslate);
+	}
+	break;
+
+	case EInterpolationType::CUBICSPLINE:
+	{
+		float deltaTime = mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex);
+		glm::vec3 prevTangent = deltaTime * mTranslations.at(prevTimeIndex * 3 + 2);
+		glm::vec3 nextTangent = deltaTime * mTranslations.at(nextTimeIndex * 3);
+
+		float interpolatedTime = (time - mTimings.at(prevTimeIndex)) /
+			(mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex));
+		float interpolatedTimeSq = interpolatedTime * interpolatedTime;
+		float interpolatedTimeCub = interpolatedTimeSq * interpolatedTime;
+
+		glm::vec3 prevPoint = mTranslations.at(prevTimeIndex * 3 + 1);
+		glm::vec3 nextPoint = mTranslations.at(nextTimeIndex * 3 + 1);
+
+		finalTranslate =
+			(2 * interpolatedTimeCub - 3 * interpolatedTimeSq + 1) * prevPoint +
+			(interpolatedTimeCub - 2 * interpolatedTimeSq + interpolatedTime) * prevTangent +
+			(-2 * interpolatedTimeCub + 3 * interpolatedTimeSq) * nextPoint +
+			(interpolatedTimeCub - interpolatedTimeSq) * nextTangent;
+
+	}
+	break;
+	}
+
+	return finalTranslate;
+}
+
+glm::vec3 GltfAnimationChannel::getScaling(float time)
+{
+	// sanity check
+	if (mScaling.size() == 0) return glm::vec3(1.0f);
+
+	if (time < mTimings.at(0)) return mScaling.at(0);
+	if (time > mTimings.at(mTimings.size() - 1)) return mScaling.at(mScaling.size() - 1);
+	
+
+	int prevTimeIndex = 0;
+	int nextTimeIndex = 0;
+	for (int i = 0; i < mTimings.size(); ++i)
+	{
+		if (mTimings.at(i) > time)
+		{
+			nextTimeIndex = i;
+			break;
+		}
+		prevTimeIndex = i;
+	}
+
+	if (prevTimeIndex == nextTimeIndex) return mScaling.at(prevTimeIndex);
+
+	glm::vec3 finalScale = glm::vec3(1.0f);
+
+	switch (mInterType)
+	{
+	case EInterpolationType::STEP: finalScale = mScaling.at(prevTimeIndex); break;
+	case EInterpolationType::LINEAR: {
+		float interpolatedTime = (time - mTimings.at(prevTimeIndex)) / (mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex));
+		glm::vec3 prevScale = mScaling.at(prevTimeIndex);
+		glm::vec3 nextScale = mScaling.at(nextTimeIndex);
+		finalScale = prevScale + interpolatedTime * (nextScale - prevScale);
+		break;
+		}
+	case EInterpolationType::CUBICSPLINE:
+	{
+		float deltaTime = mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex);
+		glm::vec3 prevTangent = deltaTime * mScaling.at(prevTimeIndex * 3 + 2);
+		glm::vec3 nextTangent = deltaTime * mScaling.at(nextTimeIndex * 3);
+		float interpolatedTime = (time - mTimings.at(prevTimeIndex)) / (mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex));
+		float interpolatedTimeSq = interpolatedTime * interpolatedTime;
+		float interpolatedTimeCub = interpolatedTimeSq * interpolatedTime;
+		glm::vec3 prevPoint = mScaling.at(prevTimeIndex * 3 + 1);
+		glm::vec3 nextPoint = mScaling.at(nextTimeIndex * 3 + 1);
+
+		finalScale =
+			(2 * interpolatedTimeCub - 3 * interpolatedTimeSq + 1) * prevPoint +
+			(interpolatedTimeCub - 2 * interpolatedTimeSq + interpolatedTime) * prevTangent +
+			(-2 * interpolatedTimeCub + 3 * interpolatedTimeSq) * nextPoint +
+			(interpolatedTimeCub - interpolatedTimeSq) * nextTangent;
+	}
+	}
+	return finalScale;
+
+}
+
+glm::quat GltfAnimationChannel::getRotation(float time)
+	{
+
+	if (mRotations.size() == 0) return glm::identity<glm::quat>();
+	if (time < mTimings.at(0)) return mRotations.at(0);
+	if (time > mTimings.at(mTimings.size() - 1)) return mRotations.at(mTimings.size() - 1);
+
+
+	int prevTimeIndex = 0;
+	int nextTimeIndex = 0;
+	for (int i = 0; i < mTimings.size(); ++i)
+	{
+		if (mTimings.at(i) > time)
+		{
+			nextTimeIndex = i;
+			break;
+		}
+		prevTimeIndex = i;
+	}
+
+	if (prevTimeIndex == nextTimeIndex) return mRotations.at(prevTimeIndex);
+
+	glm::quat finalRotate = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+
+	switch (mInterType)
+	{
+	case EInterpolationType::STEP: { finalRotate = mRotations.at(prevTimeIndex); break; }
+	case EInterpolationType::LINEAR: {
+		float interpolatedTime = (time - mTimings.at(prevTimeIndex)) / (mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex));
+		glm::quat prevRotate = mRotations.at(prevTimeIndex);
+		glm::quat nextRotate = mRotations.at(nextTimeIndex);
+		finalRotate = glm::slerp(prevRotate, nextRotate, interpolatedTime);
+		
+	}
+								   break;
+	case EInterpolationType::CUBICSPLINE :
+	{
+
+		float deltaTime = mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex);
+		glm::quat prevTangent = deltaTime * mRotations.at(prevTimeIndex * 3 + 2);
+		glm::quat nextTangent = deltaTime * mRotations.at(nextTimeIndex * 3);
+
+		float interpolatedTime = (time - mTimings.at(prevTimeIndex)) /
+			(mTimings.at(nextTimeIndex) - mTimings.at(prevTimeIndex));
+		float interpolatedTimeSq = interpolatedTime * interpolatedTime;
+		float interpolatedTimeCub = interpolatedTimeSq * interpolatedTime;
+
+		glm::quat prevPoint = mRotations.at(prevTimeIndex * 3 + 1);
+		glm::quat nextPoint = mRotations.at(nextTimeIndex * 3 + 1);
+
+		finalRotate =
+			(2 * interpolatedTimeCub - 3 * interpolatedTimeSq + 1) * prevPoint +
+			(interpolatedTimeCub - 2 * interpolatedTimeSq + interpolatedTime) * prevTangent +
+			(-2 * interpolatedTimeCub + 3 * interpolatedTimeSq) * nextPoint +
+			(interpolatedTimeCub - interpolatedTimeSq) * nextTangent;
+	}
+	break;							 
+	}
+	return finalRotate;
+
+	}
